a9d0bfc9a74a5d4c7f4aeee90ff432983c05537f
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2022-11-29 19:40:38 +0530

    latest code

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -65,9 +65,9 @@ const Banner = () => {
               <TrackVisibility>
                 {({ isVisible }) => (
                   <div
-                  // className={
-                  //   isVisible ? "animate__animated animate__fadeIn" : ""
-                  // }
+                    className={
+                      isVisible ? "animate__animated animate__fadeIn" : ""
+                    }
                   >
                     {/* <span className="tagline"> Welcome to my Portfolio</span> */}
                     <h1 className={styles.Crack}>Crack</h1>
@@ -78,53 +78,16 @@ const Banner = () => {
                     </h1>
                     <p>{info}</p>
 
-                    <span className={styles.tagline}>
-                      {/* <svg class="icon-arrow after" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 403.8 200.3" style="enable-background:new 0 0 403.8 200.3;" xml:space="preserve"> */}
-                      <style type="text/css">
-                        {/* .st0 {
-            fill: #E5EFF5;
-          } */}
-                      </style>
-                      <g>
-                        <g>
-                          <path
-                            class="st0"
-                            d="M25.2,114.3h317.3L290,166.6c-5.9,5.8-5.9,15.3-0.1,21.2c5.8,5.9,15.3,5.9,21.2,0.1l78.3-77.9c0,0,0,0,0,0
-               c5.9-5.8,5.9-15.4,0-21.2c0,0,0,0,0,0l-78.3-77.9c-5.9-5.8-15.4-5.8-21.2,0.1c-5.8,5.9-5.8,15.4,0.1,21.2l52.6,52.3H25.2
-               c-8.3,0-15,6.7-15,15C10.2,107.6,16.9,114.3,25.2,114.3z"
-                          />
-                        </g>
-                      </g>
-                      {/* </svg> */}
-                      <span>Loreum</span>
-                    </span>
+                    {/* <span className={styles.tagline}></span> */}
 
                     <span className={styles.tagline}>
                       {/* <svg class="icon-arrow after" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 403.8 200.3" style="enable-background:new 0 0 403.8 200.3;" xml:space="preserve"> */}
-                      <style type="text/css">
-                        {/* .st0 {
-            fill: #E5EFF5;
-          } */}
-                      </style>
-                      <g>
-                        <g>
-                          <path
-                            class="st0"
-                            d="M25.2,114.3h317.3L290,166.6c-5.9,5.8-5.9,15.3-0.1,21.2c5.8,5.9,15.3,5.9,21.2,0.1l78.3-77.9c0,0,0,0,0,0
-               c5.9-5.8,5.9-15.4,0-21.2c0,0,0,0,0,0l-78.3-77.9c-5.9-5.8-15.4-5.8-21.2,0.1c-5.8,5.9-5.8,15.4,0.1,21.2l52.6,52.3H25.2
-               c-8.3,0-15,6.7-15,15C10.2,107.6,16.9,114.3,25.2,114.3z"
-                          />
-                        </g>
-                      </g>
-                      {/* </svg> */}
-                      <span>Ipsum</span>
-                    </span>
-                    {/* <span className={styles.tagline}> Ipsum</span> */}
+                      <style type="text/css"></style>
 
-                    <button onClick={() => console.log("connect")}>
-                      Lets Connect{" "}
-                      <ArrowRightCircle size="25"></ArrowRightCircle>
-                    </button>
+                      <span>
+                        <p>Master the interview & land a job worth loving. </p>
+                      </span>
+                    </span>
                   </div>
                 )}
               </TrackVisibility>
